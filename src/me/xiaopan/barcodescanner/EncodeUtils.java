f78d17b74ddb96d7480b74c744d70a285a636283
ixiaopan <i@xiaopan.me>
2013-09-12 15:51:00 +0800

    基本完毕

--- src/me/xiaopan/barcodescanner/EncodeUtils.java
+++ src/me/xiaopan/barcodescanner/EncodeUtils.java
@@ -1,16 +1,25 @@
 package me.xiaopan.barcodescanner;
 
-import android.graphics.Bitmap;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.EnumMap;
+import java.util.Map;
 
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 /**
  * 编码工具箱
  */
 public class EncodeUtils {
-	private static final int WHITE = 0xFFFFFFFF;
-	private static final int BLACK = 0xFF000000;
-	
 	/**
 	 * 将BitMatrix格式的源数据转换为Bitmap
 	 * @param bitMatrix
@@ -24,12 +33,108 @@ public class EncodeUtils {
 		for (int y = 0; y < height; y++) {
 			int offset = y * width;
 			for (int x = 0; x < width; x++) {
-				pixels[offset + x] = bitMatrix.get(x, y) ? BLACK : WHITE;
+				pixels[offset + x] = bitMatrix.get(x, y) ? 0xFF000000 : 0xFFFFFFFF;
 			}
 		}
 
 		Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
 		bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
 		return bitmap;
+	}
+	
+	/**
+	 * 生成条码
+	 * @param content 条码内容
+	 * @param barcodeFormat 条码格式，可以是二维码或者一维条码
+	 * @param charset 条码内容编码方式
+	 * @param width 条码宽度
+	 * @param height 条码高度
+	 * @param hints 选项集
+	 * @param logoBitmap 可以在条码中间显示一个logologo
+	 * @param outFile 输出文件
+	 * @return Bitmap
+	 * @throws Exception
+	 */
+	public static final Bitmap encode(String content, BarcodeFormat barcodeFormat, String charset, int width, int height, Map<EncodeHintType, Object> hints, Bitmap logoBitmap, File outFile) throws Exception{
+		Bitmap qrcodeBitmap = null;
+		try {
+			if(hints == null){
+				hints = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
+			}
+			
+			//根据Logo的大小同输出二维码的宽高设置合适的容错率
+			if(logoBitmap != null){
+				int logoWidth = logoBitmap.getWidth();
+				int logoHeight = logoBitmap.getHeight();
+				if(logoWidth > logoHeight){
+					hints.put(EncodeHintType.ERROR_CORRECTION, getErrorCorrectionLevel((float) logoWidth/(float) width));
+				}else{
+					hints.put(EncodeHintType.ERROR_CORRECTION, getErrorCorrectionLevel((float) logoHeight/(float) height));
+				}
+			}
+			
+			//设置编码方式
+			if(!hints.containsKey(EncodeHintType.CHARACTER_SET)){
+				if(charset != null && !"".equals(charset.trim())){
+					hints.put(EncodeHintType.CHARACTER_SET, charset);
+				}else{
+					hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+				}
+			}
+			
+			//生成二维码
+			qrcodeBitmap = EncodeUtils.bitMatrixToBitmap(new MultiFormatWriter().encode(content, barcodeFormat, width, height, hints));
+			
+			//绘制Logo
+			if(logoBitmap != null){
+				Paint paint = new Paint();
+				Canvas canvas = new Canvas(qrcodeBitmap);
+				canvas.drawBitmap(logoBitmap, (qrcodeBitmap.getWidth() - logoBitmap.getWidth())/2, (qrcodeBitmap.getHeight() - logoBitmap.getHeight())/2, paint);
+				logoBitmap.recycle();
+			}
+			
+			//将二维码输出到本地
+			if(outFile != null){
+				if(!outFile.exists()){
+					File parentFile = outFile.getParentFile();
+					if(!parentFile.exists()){
+						parentFile.mkdirs();
+					}
+				}
+				
+				if(outFile.createNewFile()){
+					qrcodeBitmap.compress(CompressFormat.JPEG, 100, new FileOutputStream(outFile));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			if(qrcodeBitmap != null && !qrcodeBitmap.isRecycled()){
+				qrcodeBitmap.recycle();
+				qrcodeBitmap = null;
+			}
+			if(logoBitmap != null && !logoBitmap.isRecycled()){
+				logoBitmap.recycle();
+				logoBitmap = null;
+			}
+			throw e;
+		}
+		return qrcodeBitmap;
+	}
+	
+	/**
+	 * 根据logo和二维码图的大小比例获取合适的容错级别
+	 * @param proportion
+	 * @return
+	 */
+	private static final ErrorCorrectionLevel getErrorCorrectionLevel(float proportion){
+		if(proportion <= 0.07){
+			return ErrorCorrectionLevel.L;
+		}else if(proportion <= 0.15){
+			return ErrorCorrectionLevel.M;
+		}else if(proportion <= 0.25){
+			return ErrorCorrectionLevel.Q;
+		}else{
+			return ErrorCorrectionLevel.H;
+		}
 	}
 }
--- src/me/xiaopan/barcodescanner/activity/EncodeActivity.java
+++ src/me/xiaopan/barcodescanner/activity/EncodeActivity.java
@@ -1,18 +1,11 @@
 package me.xiaopan.barcodescanner.activity;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.util.EnumMap;
-import java.util.Map;
 
 import me.xiaopan.barcodescanner.EncodeUtils;
 import me.xiaopan.barcodescanner.R;
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.os.Environment;
 import android.text.TextUtils;
@@ -23,9 +16,6 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 /**
  * 编码界面
@@ -48,44 +38,11 @@ public class EncodeActivity extends Activity{
 			public void onClick(View v) {
 				String content = contentEdit.getEditableText().toString().trim();
 				if(!TextUtils.isEmpty(content)){
-					Bitmap qrcodeBitmap = null;
-					Bitmap icBitmap = null;
 					try {
-						//设置容错率，因为要在二维码上绘制logo，所以必须要有容错率，要不然二维无法识别
-						Map<EncodeHintType, Object> hints = new EnumMap<EncodeHintType, Object>(EncodeHintType.class);
-						hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
-						hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
-						
-						qrcodeBitmap = EncodeUtils.bitMatrixToBitmap(new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, 300, 300, hints));
-						icBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher, null);
-						int left = (qrcodeBitmap.getWidth() - icBitmap.getWidth())/2;
-						int top = (qrcodeBitmap.getHeight() - icBitmap.getHeight())/2;
-						Paint paint = new Paint();
-						Canvas canvas = new Canvas(qrcodeBitmap);
-						canvas.drawBitmap(icBitmap, left, top, paint);
-						icBitmap.recycle();
-						
-						File dir = new File(Environment.getExternalStorageDirectory().getPath() + File.separator + getPackageName());
-						if(!dir.exists()){
-							dir.mkdirs();
-						}
-						
-						File newFile = new File(dir.getPath() + File.separator + System.currentTimeMillis() + ".jpeg");
-						if(newFile.createNewFile()){
-							qrcodeBitmap.compress(CompressFormat.JPEG, 100, new FileOutputStream(newFile));
-							qrcodeImage.setImageBitmap(qrcodeBitmap);
-							Toast.makeText(getBaseContext(), "已保存至 "+newFile.getPath(), Toast.LENGTH_SHORT).show();
-						}else{
-							throw new Exception();
-						}
+						qrcodeImage.setImageBitmap(EncodeUtils.encode(content, BarcodeFormat.QR_CODE, null, 400, 400, null, BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher, null), new File(new File(Environment.getExternalStorageDirectory().getPath() + File.separator + getPackageName()).getPath() + File.separator + System.currentTimeMillis() + ".jpeg")));
+//						qrcodeImage.setImageBitmap(EncodeUtils.encode(content, BarcodeFormat.CODE_39, null, 200, 100, null, null, new File(new File(Environment.getExternalStorageDirectory().getPath() + File.separator + getPackageName()).getPath() + File.separator + System.currentTimeMillis() + ".jpeg")));
 					} catch (Exception e) {
 						e.printStackTrace();
-						if(qrcodeBitmap != null && !qrcodeBitmap.isRecycled()){
-							qrcodeBitmap.recycle();
-						}
-						if(icBitmap != null && !icBitmap.isRecycled()){
-							icBitmap.recycle();
-						}
 						Toast.makeText(getBaseContext(), "生成失败", Toast.LENGTH_SHORT).show();
 					}
 				}else{
